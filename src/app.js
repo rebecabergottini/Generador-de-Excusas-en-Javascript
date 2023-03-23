0cd02cdfcf395885be410962c9a9d978390508a0
rebecabergottini <117315880+rebecabergottini@users.noreply.github.com>
2023-03-23 12:08:56 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const excuseElement = document.getElementById("excuse");
+
+const who = ["The dog", "My granma", "His turtle", "My bird"];
+const what = ["eat", "pissed", "crushed", "broked"];
+const when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  let randomWhoIndex = Math.floor(Math.random() * who.length);
+  let randomWhatIndex = Math.floor(Math.random() * what.length);
+  let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+  const excuse = `${who[randomWhoIndex]} ${what[randomWhatIndex]} ${when[randomWhenIndex]}`;
+
+  return excuse;
+}
+
+excuseElement.innerHTML = generateExcuse();
